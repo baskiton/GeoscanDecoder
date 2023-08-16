d1e78adefd4dcb8761274a95e22c308ad1347413
Vladimir <60815271+rmdlv@users.noreply.github.com>
2023-08-16 18:05:02 +0800

    Revert "Close the previous image file before creating a new one"
    This reverts commit 5fd47fd086066afcfb97afecd12bae3cf3ecb01a.

--- GeoscanDecoder/__main__.py
+++ GeoscanDecoder/__main__.py
@@ -6,55 +6,37 @@ from GeoscanDecoder import CONFIG, HOMEDIR, ui
 from GeoscanDecoder.version import __version__
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     cp = configparser.ConfigParser()
-    cp.read_dict(
-        {
-            "main": {
-                "ip": "127.0.0.1",
-                "port": "8000",
-                "outdir": str(HOMEDIR),
-                "merge mode": "off",
-            },
-            "info": {"version": __version__},
-        }
-    )
+    cp.read_dict({'main': {'ip': '127.0.0.1',
+                           'port': '8000',
+                           'outdir': str(HOMEDIR),
+                           'merge mode': 'off'},
+                  'info': {'version': __version__}})
     cp.read(CONFIG)
 
     ap = argparse.ArgumentParser()
 
-    ap.add_argument(
-        "--server", default=cp.get("main", "ip"), help="Soundmodem connection IP"
-    )
-    ap.add_argument(
-        "--port", default=cp.get("main", "port"), help="Soundmodem connection port"
-    )
-    ap.add_argument(
-        "--outdir",
-        default=cp.get("main", "outdir"),
-        help="Directory to store received data",
-    )
-    ap.add_argument(
-        "--merge",
-        default=cp.get("main", "merge mode"),
-        help="Store all new images data to one file",
-    )
-    ap.add_argument("--ui", help="Run in GUI", action="store_true")
+    ap.add_argument('--server', default=cp.get('main', 'ip'), help='Soundmodem connection IP')
+    ap.add_argument('--port', default=cp.get('main', 'port'), help='Soundmodem connection port')
+    ap.add_argument('--outdir', default=cp.get('main', 'outdir'), help='Directory to store received data')
+    ap.add_argument('--merge', default=cp.get('main', 'merge mode'), help='Store all new images data to one file')
+    ap.add_argument('--ui', help='Run in GUI', action='store_true')
 
     args = ap.parse_args()
-    cp.set("main", "ip", args.server or "127.0.0.1")
-    cp.set("main", "port", args.port or "8000")
-    cp.set("main", "outdir", str(args.outdir) or str(HOMEDIR))
-    cp.set("main", "merge mode", args.merge or "off")
+    cp.set('main', 'ip', args.server or '127.0.0.1')
+    cp.set('main', 'port', args.port or '8000')
+    cp.set('main', 'outdir', str(args.outdir) or str(HOMEDIR))
+    cp.set('main', 'merge mode', args.merge or 'off')
 
-    frozen = getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS")
+    frozen = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
 
     if args.ui or frozen:
         app = ui.App(cp)
         app.mainloop()
     else:
-        print("Console version not supported yet", file=sys.stderr)
+        print('Console version not supported yet', file=sys.stderr)
         sys.exit(-1)
 
-    with CONFIG.open("w") as cf:
+    with CONFIG.open('w') as cf:
         cp.write(cf)
--- GeoscanDecoder/geoscan.py
+++ GeoscanDecoder/geoscan.py
@@ -5,21 +5,20 @@ import construct
 
 
 ssid = construct.BitStruct(
-    "ch" / construct.Flag,  # C / H bit
+    'ch' / construct.Flag,  # C / H bit
     construct.Default(construct.BitsInteger(2), 3),  # reserved bits
-    "ssid" / construct.BitsInteger(4),
-    "extension" / construct.Flag,  # last address bit
+    'ssid' / construct.BitsInteger(4),
+    'extension' / construct.Flag  # last address bit
 )
 
 
 class CallsignAdapter(construct.Adapter):
     def _encode(self, obj, context, path=None):
-        return bytes(
-            [x << 1 for x in bytes((obj.upper() + " " * 6)[:6], encoding="ascii")]
-        )
+        return bytes([x << 1 for x in bytes(
+            (obj.upper() + ' '*6)[:6], encoding='ascii')])
 
     def _decode(self, obj, context, path=None):
-        return str(bytes([x >> 1 for x in obj]), encoding="ascii").strip()
+        return str(bytes([x >> 1 for x in obj]), encoding='ascii').strip()
 
 
 class UNIXTimestampAdapter(construct.Adapter):
@@ -32,19 +31,25 @@ class UNIXTimestampAdapter(construct.Adapter):
 
 callsign = CallsignAdapter(construct.Bytes(6))
 
-address = construct.Struct("callsign" / callsign, "ssid" / ssid)
+address = construct.Struct(
+        'callsign' / callsign,
+        'ssid' / ssid
+)
 
 control = construct.Hex(construct.Int8ub)
 
 pid = construct.Hex(construct.Int8ub)
 
 ax25_header = construct.Struct(
-    "addresses" / construct.RepeatUntil(lambda x, lst, ctx: x.ssid.extension, address),
-    "control" / control,
-    "pid" / pid,
+    'addresses' / construct.RepeatUntil(lambda x, lst, ctx: x.ssid.extension, address),
+    'control' / control,
+    'pid' / pid
 )
 
-ax25 = construct.Struct("header" / ax25_header, "info" / construct.GreedyBytes)
+ax25 = construct.Struct(
+    'header' / ax25_header,
+    'info' / construct.GreedyBytes
+)
 
 
 # GEOSCAN Telemetry Protocol
@@ -77,47 +82,43 @@ class MulAdapter(construct.Adapter):
 
 
 geoscan_frame = construct.Struct(
-    "time" / UNIXTimestampAdapter(construct.Int32ul),
-    "Iab" / MulAdapter(0.0766, construct.Int16ul),  # mA
-    "Isp" / MulAdapter(0.03076, construct.Int16ul),  # mA
-    "Uab_per" / MulAdapter(0.00006928, construct.Int16ul),  # V
-    "Uab_sum" / MulAdapter(0.00013856, construct.Int16ul),  # V
-    "Tx_plus" / construct.Int8ul,  # deg C
-    "Tx_minus" / construct.Int8ul,  # deg C
-    "Ty_plus" / construct.Int8ul,  # deg C
-    "Ty_minus" / construct.Int8ul,  # deg C
-    "Tz_plus" / construct.Int8ul,  # undef
-    "Tz_minus" / construct.Int8ul,  # deg C
-    "Tab1" / construct.Int8ul,  # deg C
-    "Tab2" / construct.Int8ul,  # deg C
-    "CPU_load" / construct.Int8ul,  # %
-    "Nres_osc" / SubAdapter(7476, construct.Int16ul),
-    "Nres_CommU" / SubAdapter(1505, construct.Int16ul),
-    "RSSI" / SubAdapter(99, construct.Int8ul),  # dBm
-    "pad" / construct.GreedyBytes,
+    'time' / UNIXTimestampAdapter(construct.Int32ul),
+    'Iab' / MulAdapter(0.0766, construct.Int16ul),      # mA
+    'Isp' / MulAdapter(0.03076, construct.Int16ul),     # mA
+
+    'Uab_per' / MulAdapter(0.00006928, construct.Int16ul),   # V
+    'Uab_sum' / MulAdapter(0.00013856, construct.Int16ul),   # V
+
+    'Tx_plus' / construct.Int8ul,   # deg C
+    'Tx_minus' / construct.Int8ul,  # deg C
+    'Ty_plus' / construct.Int8ul,   # deg C
+    'Ty_minus' / construct.Int8ul,  # deg C
+    'Tz_plus' / construct.Int8ul,   # undef
+    'Tz_minus' / construct.Int8ul,  # deg C
+    'Tab1' / construct.Int8ul,      # deg C
+    'Tab2' / construct.Int8ul,      # deg C
+    'CPU_load' / construct.Int8ul,  # %
+    'Nres_osc' / SubAdapter(7476, construct.Int16ul),
+    'Nres_CommU' / SubAdapter(1505, construct.Int16ul),
+    'RSSI' / SubAdapter(99, construct.Int8ul),  # dBm
+
+    'pad' / construct.GreedyBytes
 )
 
 geoscan = construct.Struct(
-    "ax25" / construct.Peek(ax25_header),
-    "ax25"
-    / construct.If(
-        lambda this: (bool(this.ax25) and this.ax25.addresses[0].callsign == "BEACON"),
-        ax25_header,
-    ),
-    "geoscan"
-    / construct.If(
-        lambda this: (bool(this.ax25) and this.ax25.pid == 0xF0), geoscan_frame
-    ),
+    'ax25' / construct.Peek(ax25_header),
+    'ax25' / construct.If(lambda this: (bool(this.ax25) and this.ax25.addresses[0].callsign == u'BEACON'), ax25_header),
+    'geoscan' / construct.If(lambda this: (bool(this.ax25) and this.ax25.pid == 0xF0), geoscan_frame),
 )
 
 
 _frame = construct.Struct(
-    "marker" / construct.Int16ul,  # #0
-    "dlen" / construct.Int8ul,  # #2
-    "mtype" / construct.Int16ul,  # #3
-    "offset" / construct.Int16ul,  # #5
-    "subsystem_num" / construct.Int8ul,  # #7
-    "data" / construct.Bytes(construct.this.dlen - 6)
+    'marker' / construct.Int16ul,           # #0
+    'dlen' / construct.Int8ul,              # #2
+    'mtype' / construct.Int16ul,            # #3
+    'offset' / construct.Int16ul,           # #5
+    'subsystem_num' / construct.Int8ul,     # #7
+    'data' / construct.Bytes(construct.this.dlen - 6)
     # 'data' / construct.Bytes(56)
 )
 
@@ -126,7 +127,7 @@ class GeoscanImageReceiver:
     MARKER_IMG = 0x0001
     CMD_IMG_START = 0x0901
     CMD_IMG_FRAME = 0x0905
-    BASE_OFFSET = 0  # old 4     # old 16384     # old 32768
+    BASE_OFFSET = 0     # old 4     # old 16384     # old 32768
 
     def __init__(self, outdir):
         self.outdir = pathlib.Path(outdir).expanduser().absolute()
@@ -148,21 +149,16 @@ class GeoscanImageReceiver:
 
     def generate_fid(self):
         if not (self.current_fid and self.merge_mode):
-            self.current_fid = f"GEOSCAN_{dt.datetime.now()}".replace(" ", "_").replace(
-                ":", "-"
-            )
+            self.current_fid = f'GEOSCAN_{dt.datetime.now()}'.replace(' ', '_').replace(':', '-')
         return self.current_fid
 
     def force_new(self):
-        f = self.files.get(self.current_fid)
-        if f:
-            f.close()
         self.has_starter = self.has_soi = self.current_fid = 0
         self.new_file(self.generate_fid())
 
     def new_file(self, fid):
-        fn = self.outdir / (fid + ".jpg")
-        f = open(fn, "w+b")
+        fn = self.outdir / (fid + '.jpg')
+        f = open(fn, 'w+b')
         self.files[fid] = f
         return f
 
@@ -206,7 +202,7 @@ class GeoscanImageReceiver:
             return
 
         if data.mtype == self.CMD_IMG_START:
-            if data.data.startswith(b"\xff\xd8"):
+            if data.data.startswith(b'\xff\xd8'):
                 self.has_soi = data.offset
             self.has_starter = 1
             self.base_offset = data.offset
@@ -214,11 +210,9 @@ class GeoscanImageReceiver:
             self.generate_fid()
 
         elif data.mtype == self.CMD_IMG_FRAME:
-            if (
-                not self.has_starter
-                and not self.has_soi
-                and data.data.startswith(b"\xff\xd8")
-            ):
+            if (not self.has_starter
+                    and not self.has_soi
+                    and data.data.startswith(b'\xff\xd8')):
                 self.base_offset = data.offset
                 self.has_soi = data.offset
                 self.generate_fid()
@@ -238,4 +232,4 @@ class GeoscanImageReceiver:
     def is_last_data(self, data):
         prev_sz = self._prev_data_sz
         self._prev_data_sz = len(data.data)
-        return (self._prev_data_sz < prev_sz) and b"\xff\xd9" in data.data
+        return (self._prev_data_sz < prev_sz) and b'\xff\xd9' in data.data
